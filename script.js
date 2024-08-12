a583d612e3381d91dcb275c408b32827012729f1
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 12:40:58 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,71 @@
+console.log("Script running...")
+// Task 1: Hello button flow
+// Select the 'Click me for hello' button
+
+let button = document.querySelector("#helloButton")
+console.log(button)
+
+// Select the empty p tag under that button
+
+let tag = document.querySelector("#helloText")
+console.log(tag)
+// Add an event listener to the hello button such that when it's click, the p tag text says "Hello"
+
+button.addEventListener("click",function(event){
+tag.textContent = "Hello"
+})
+
+// Task 2: All caps button flow
+// Select the 'Click me for all caps' button
+
+let caps = document.querySelector("#capsButton")
+
+// Select the p tag under that button (says there's no need to shout)
+
+let cText = document.querySelector("#capsText")
+
+// Add an event listener to the caps button such that when it's clicked, the p tag text is "THERE'S NO NEED TO SHOUT"
+
+caps.addEventListener("click",function(event){
+  cText.textContent = "THERE'S NO NEED TO SHOUT"
+})
+
+
+// Task 3: Dog image flow
+// Select the dog image
+
+let dogImage = document.querySelector(".dogImage")
+
+// Add an event listener to the dog image such that when the mouse is over it, the image src becomes happy-dog.jpeg
+dogImage.addEventListener('mouseover', function(event){
+  dogImage.src = "happy-dog.jpeg"
+})
+
+
+// Add an event listener to the dog image such that when the mouse is off it, the image src becomes sad-dog.jpeg
+
+dogImage.addEventListener('mouseover', function(event){
+  dogImage.src = "sad-dog.jpeg"
+})
+
+
+// Task 4: Name input flow
+// Select the submit button
+
+let submit = document.querySelector("#nameButton")
+
+// Select the input 
+let input = document.querySelector("#input")
+
+// Select the empty p tag under them
+
+let name = document.querySelector("#nameText")
+
+
+// Add an event listener to the submit button such that when it is clicked, the p tag text is "Nice to meet you " + input.value
+
+submit.addEventListener("click",function(event){
+name.textContent = "Nice to meet you" + input.value
+})
+
+
